b81f8137ab803580aa371a6c04c8e124b14492ac
Abhisharma08 <aksharma223@gmail.com>
2026-04-23 16:33:57 +0530

    feat: add HubSpot API route handler to sync contact form submissions

--- apps/web/api/hubspot/index.js
+++ apps/web/api/hubspot/index.js
@@ -1,4 +1,4 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
@@ -52,4 +52,4 @@ module.exports = async function handler(req, res) {
     console.error("Server error:", error);
     res.status(500).json({ error: "Failed to send data" });
   }
-};
+}
